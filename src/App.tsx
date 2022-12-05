90ebc728590050e7be9ef3a3e5e91972b92c98dd
Bruno Garcia <bruno.garcia@wallbox.com>
2022-12-05 15:48:27 +0100

    feat: check if the wallet is connected

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+
 import './styles/App.css';
 
 // Constants
@@ -6,12 +7,51 @@ const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
 const App = () => {
+  /*
+  * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
+  */
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  /*
+  * Gotta make sure this is async.
+  */
+  const checkIfWalletIsConnected = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+    } else {
+        console.log("We have the ethereum object", ethereum);
+    }
+
+   /*
+    * Check if we're authorized to access the user's wallet
+    */
+   const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+    /*
+    * User can have multiple authorized accounts, we grab the first one if its there!
+    */
+    if (accounts && Array.isArray(accounts) && accounts.length !== 0) {
+      const account = accounts[0];
+      console.log("Found an authorized account:", account);
+      setCurrentAccount(account);
+    } else {
+      console.log("No authorized account found");
+    }
+  }
+
   // Render Methods
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button">
       Connect to Wallet
     </button>
   );
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
 
   return (
     <div className="App">
@@ -24,7 +64,12 @@ const App = () => {
           {renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
-          Bruno Garcia
+          <a
+            className="footer-text"
+            href="https://github.com/brunogarcia"
+            target="_blank"
+            rel="noreferrer"
+          >Bruno Garcia</a>
         </div>
       </div>
     </div>
